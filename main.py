14f5707c9e0ec8948d1df0bae837706df9b6bdf7
jamliddin123 <jamoliddinshodmonov89@gmail.com>
2024-07-04 23:52:09 +0500

    the file succesfully added

---
+++ main.py
@@ -0,0 +1,28 @@
+class Fibonacci:
+    def __init__(self, max_value=100):
+        self.max_value = max_value
+
+    def __iter__(self):
+        self.current = 0
+        self.next_value = 1
+        return self
+
+    def __next__(self):
+        if self.current > self.max_value:
+            raise StopIteration
+        value_to_return = self.current
+        self.current, self.next_value = self.next_value, self.current + self.next_value
+        return value_to_return
+
+    def save_to_file(self, filename):
+        with open(filename, 'w') as f:
+            for number in self:
+                f.write(f"{number}\n")
+
+
+fib = Fibonacci(100)
+
+for num in fib:
+    print(num)
+
+fib.save_to_file('fibonacci.txt')
